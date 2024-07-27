d7603b19c3cec597cd739037556ee816c0837ddf
Jared Grogan <33227815+jaredgrogan@users.noreply.github.com>
2024-07-26 23:18:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -3,28 +3,48 @@ document.addEventListener('DOMContentLoaded', () => {
     const modeToggle = document.querySelector('.mode-toggle');
     const modeIcon = document.querySelector('.mode-icon');
     const typingText = document.getElementById('typing-text');
+    const centerLogo = document.getElementById('center-logo');
 
     function toggleMode() {
         body.classList.toggle('night-mode');
         body.classList.toggle('day-mode');
         updateModeIcon();
+        updateCenterLogo();
     }
 
     function updateModeIcon() {
         const isNightMode = body.classList.contains('night-mode');
         modeIcon.innerHTML = isNightMode ? getSunIcon() : getMoonIcon();
+        modeIcon.style.fill = isNightMode ? 'white' : 'black';
     }
 
     function getSunIcon() {
-        return `<circle cx="12" cy="12" r="5" /><line x1="12" y1="1" x2="12" y2="3" /><line x1="12" y1="21" x2="12" y2="23" /><line x1="4.22" y1="4.22" x2="5.64" y2="5.64" /><line x1="18.36" y1="18.36" x2="19.78" y2="19.78" /><line x1="1" y1="12" x2="3" y2="12" /><line x1="21" y1="12" x2="23" y2="12" /><line x1="4.22" y1="19.78" x2="5.64" y2="18.36" /><line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />`;
+        return `
+            <circle cx="12" cy="12" r="5" />
+            <line x1="12" y1="1" x2="12" y2="3" />
+            <line x1="12" y1="21" x2="12" y2="23" />
+            <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+            <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+            <line x1="1" y1="12" x2="3" y2="12" />
+            <line x1="21" y1="12" x2="23" y2="12" />
+            <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+            <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+        `;
     }
 
     function getMoonIcon() {
         return `<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />`;
     }
 
+    function updateCenterLogo() {
+        const isNightMode = body.classList.contains('night-mode');
+        const vColor = isNightMode ? 'white' : 'black';
+        centerLogo.querySelector('text').setAttribute('fill', vColor);
+    }
+
     modeToggle.addEventListener('click', toggleMode);
     updateModeIcon();
+    updateCenterLogo();
 
     function typeText(text, element, speed = 100) {
         let i = 0;
